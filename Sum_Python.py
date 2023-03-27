83735bd2a430ef4ee72f965db6c6176e5304db0c
Esteban1997mene <128060588+Esteban1997mene@users.noreply.github.com>
2023-03-27 09:07:07 -0500

    Create Sum_Python.py

---
+++ Sum_Python.py
@@ -0,0 +1,11 @@
+print("SUMA")
+Num_1 = 5
+Num_2 = 4
+resultado = Num_1+Num_2
+print("El resultado es:"+str(resultado))
+
+print("RESTA")
+Num_1 = 5
+Num_2 = 4
+resultado = Num_1-Num_2
+print("El resultado es:"+str(resultado))
